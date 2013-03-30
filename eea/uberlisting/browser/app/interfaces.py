7c26b10eaebd603427f00fa3097f753980852139
ichim-david <ichim.david@gmail.com>
2013-03-30 18:23:41 +0200

    Added eea.uberlisting.configure permission; whitespace cleanup

--- eea/uberlisting/browser/app/interfaces.py
+++ eea/uberlisting/browser/app/interfaces.py
@@ -5,5 +5,5 @@ from zope.interface import Interface
 
 
 class IUberlistingView(Interface):
-    """Marker interface to indicate that SubFoldersViewlet should be enabled
+    """Marker interface to indicate that UberlistingView is enabled
     """
