2c41706bd3cae32f0a9168bf17b92655de0e69c5
ichim-david <ichim.david@gmail.com>
2013-04-02 10:44:52 +0300

    Changed the following: - Added z3c.form for editing of properties that can be enabled for uberlisting - Added adaptor for IUberlistingView in order for the form to work - Added availableLayoutForContext vocabulary

---
+++ eea/uberlisting/browser/app/vocabularies.py
@@ -0,0 +1,25 @@
+""" Custom vocabularies
+"""
+
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.interface import alsoProvides
+
+
+def available_context_layouts(context):
+    """ :param context: Plone context object
+        :return: Available layouts for given object
+    """
+    terms = []
+    layouts = context.getAvailableLayouts()
+    for layout in layouts:
+        if layout[0] == 'uberlisting_view':
+            continue
+        terms.append(SimpleVocabulary.createTerm(layout[0], str(layout[0]),
+                                                 layout[1]))
+
+    return SimpleVocabulary(terms)
+
+
+alsoProvides(available_context_layouts, IContextSourceBinder)
+
