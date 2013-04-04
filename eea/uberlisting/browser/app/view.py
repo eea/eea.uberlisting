455d669f846eafc2ad6c9a87e2df4212ad592f3c
ichim-david <ichim.david@gmail.com>
2013-04-04 09:17:04 +0200

    Use content-core instead of listing macro for default template macro which is loaded from the uberlisting template

--- eea/uberlisting/browser/app/view.py
+++ eea/uberlisting/browser/app/view.py
@@ -76,7 +76,7 @@ class UberlistingView(BrowserView):
         """
         template = self.getTemplate()
         if template:
-            macro = template.macros.get('listing')
+            macro = template.macros.get('content-core')
             if macro:
                 return macro
         error_view = getattr(self.context, 'macro_error_view')
